a0549a6f731f38c4c1174ce28e2ffd637fef5791
Diksha Chakraborty <74681352+letdeeseecode@users.noreply.github.com>
2025-11-24 20:06:58 +0530

    Implementing TCP client

---
+++ assignment_1/tcpclient.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+int main(){
+
+  int clientSocket;
+  char buffer[1024]; 
+  struct sockaddr_in serverAddr, clientAddr;
+  socklen_t addr_size;
+
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(7891);
+  serverAddr.sin_addr.s_addr = inet_addr("10.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  strcpy(buffer, "Hello Server, I'm client\n");
+
+  send(clientSocket,buffer, 1024, 0);
+  recv(clientSocket, buffer, 1024, 0);
+
+  printf("\nData received from server: %s\n",buffer);   
+
+
+  return 0;
+
+}
